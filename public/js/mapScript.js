e8eb33675e291bb39c3be59975bad8d33e53467b
shaunsnowden <ssnowden07@gmail.com>
2017-11-08 00:06:25 -0700

    Updated Modals and built out landing page

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -1,43 +1,18 @@
 console.log("\n mapScript js LOADED!!!!");
 
-var map;
-var infowindow;
-
 function initMap() {
-  var denver = {lat: 39.7392, lng: -104.9903};
-
-  map = new google.maps.Map(document.getElementById('mapDivTarget'), {
-    center: denver,
-    zoom: 15
+  var denver = { lat: 39.7392, lng: -104.9903 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: denver
   });
-
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  // service.nearbySearch({
-  //   location: denver,
-  //   radius: 10,
-  //   type: ['store']
-  // }, callback);
-}
-
-function callback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-      console.log(results[i]);
-    }
-  }
+  var marker = new google.maps.Marker({
+    position: denver,
+    map: map
+  });
 };
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
